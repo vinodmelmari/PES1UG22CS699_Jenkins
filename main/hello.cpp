14d133751ef7bf688d5337b7f5a48039d1edfb74
vinodmelmari <vinodmelmari@gmail.com>
2025-03-23 17:16:10 +0530

    pes1ug22cs699

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG22CS699 have successfully built and run this  \n";
+  return 0;
+}
