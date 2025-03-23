819aa00ce2c5250ec708b15fa64ed992142e9461
vinodmelmari <vinodmelmari@gmail.com>
2025-03-23 18:40:09 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG22CS699 have successfully built and run this  \n";
+  cout << "PES1UG22CS699 have made changes and  successfully built and run this  \n";
   return 0;
 }
